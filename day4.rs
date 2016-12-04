7be4fc9d1c9ea9985b09be0b5129f02157432dde
kjempelodott <kjempelodott@gmail.com>
2016-12-04 18:02:02 +0100

    use VecDeque

--- day4.rs
+++ day4.rs
@@ -1,15 +1,16 @@
+use std::collections::VecDeque;
 const N: usize = 1337;
 
 fn main() {
-    let mut n: Vec<usize> = (1..N+1)
-        .map(|i| if i % 7 != 0 && !i.to_string().contains('7') { return i } else { 0 } )
-        .collect();
-    println!("{}", (0..N)
-             .scan(0, |j, i| {
-                 if n[i] == 0 {
-                     n[i] = n[*j];
-                     *j += 1;
-                 }
-                 Some(n[i])
-             }).last().unwrap());
+    let mut n: VecDeque<usize> = VecDeque::new();
+    for i in 1..N+1 {
+        if i % 7 != 0 && !i.to_string().contains('7') {
+            n.push_back(i);
+        }
+        else {
+            let m = n.pop_front().unwrap();
+            n.push_back(m);
+        }
+    }
+    println!("{}", n.back().unwrap());
 }
